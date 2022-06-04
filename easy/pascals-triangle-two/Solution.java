bd2606219e0fd824d73068894dcf3ca6541fe300
ud17 <upandya1297@gmail.com>
2022-06-03 20:34:59 -0400

    easy: pascals-triangle-two

---
+++ easy/pascals-triangle-two/Solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public List<Integer> getRow(int numRows) {
+        
+        List<List<Integer>> response = new LinkedList();
+        numRows++;
+        
+        for(int i=0; i<numRows; i++) {
+            
+            List<Integer> curr = new ArrayList<>();
+            if(i==0) {
+                curr.add(1);
+            } else if(i==1) {
+                curr.add(1);
+                curr.add(1);
+            } else {
+                List<Integer> dummy = response.get(i-1);
+                curr.add(1);
+                for(int j=0; j<dummy.size(); j++) {
+                    int val = 0;
+                    if(j+1<dummy.size()) {
+                        val += dummy.get(j)+dummy.get(j+1);
+                    } else {
+                        val += dummy.get(j);
+                    }
+                    curr.add(val);
+                }
+            }
+            response.add(curr);
+        }
+    
+        return response.get(numRows-1);
+    }
+}
