319628ea5c9827b69be9aae36db6013887eeec6f
ud17 <upandya1297@gmail.com>
2022-06-20 14:44:39 -0400

    easy: valid-perfect-square

---
+++ easy/valid-perfect-square/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+
+        int low = 0;
+        int high = num;
+        while(low<=high) {
+            long mid = low + (high-low)/2;
+            if(mid*mid == num) return true;
+            
+            if(mid*mid>num) high = (int)mid-1;
+            else low = (int)mid+1;
+        }
+        return false;
+    }
+}
