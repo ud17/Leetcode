403fc511771e72784a508a218eb7354502412ec7
ud17 <upandya1297@gmail.com>
2022-08-06 12:35:35 -0400

    easy: design-hash-set (solution-two)

--- easy/design-hash-set/Solution.java
+++ easy/design-hash-set/Solution.java
@@ -1,61 +1,36 @@
-public class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        
-        ListNode head = new ListNode(0);
-        ListNode dummy = head;
-        
-        for(ListNode list: lists) {
-            while(list != null) {
-                head.next = new ListNode(list.val);
-                head = head.next;
-                list = list.next;
+public class MyHashSet {
+
+    ListNode head = null;
+    
+    public MyHashSet() {
+        head = new ListNode(Integer.MAX_VALUE);
+    }
+    
+    public void add(int key) {
+        ListNode pointer = head;
+        if(!contains(key)) {
+            while(pointer.next!=null) {
+                pointer = pointer.next;
             }
+            pointer.next = new ListNode(key);
         }
-        
-        return mergeSort(dummy.next);
     }
     
-    private static ListNode mergeSort(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+    public void remove(int key) {
+        ListNode pointer = new ListNode(0);
+        pointer.next = head;
+        while(pointer.next!=null) {
+            if(pointer.next.val == key) pointer.next = pointer.next.next;
+            else pointer = pointer.next;
         }
-        ListNode middle = getMiddle(head);
-        ListNode nextOfMiddle = middle.next;
-        middle.next = null;
-        ListNode left = mergeSort(head); // apply merge sort on left half
-        ListNode right = mergeSort(nextOfMiddle); // apply merge sort on righ half
-        return mergeTwoNode(left, right);
     }
     
-    private static ListNode mergeTwoNode(ListNode a, ListNode b) {
-        ListNode result = null;
-        // base cases
-        if (a == null) {
-            return b;
+    public boolean contains(int key) {
+        ListNode pointer = head;
+        while(pointer!=null) {
+            if(pointer.val == key) return true;
+            pointer = pointer.next;
         }
-        if (b == null) {
-            return a;
-        }
-        if (a.val <= b.val) {
-            result = a;
-            result.next = mergeTwoNode(a.next, b);
-        } else {
-            result = b;
-            result.next = mergeTwoNode(a, b.next);
-        }
-        return result;
-    }
-    
-    private static ListNode getMiddle(ListNode head) {
-        if (head == null) {
-            return head;
-        } 
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
+        return false;
     }
 }
---
+++ easy/design-hash-set/SolutionTwo.java
@@ -0,0 +1,24 @@
+public class MyHashSet {
+
+    int[] arr = new int[1000001];
+   
+    public MyHashSet() {
+         Arrays.fill(arr,-1);
+    }
+    
+    public void add(int key) {
+        arr[key] = key;
+    }
+    
+    public void remove(int key) {
+        arr[key]=-1;
+    }
+    
+    public boolean contains(int key) {
+        int ans = arr[key];
+        if(ans==-1){
+            return false;
+        }
+        return true;
+    }
+}
