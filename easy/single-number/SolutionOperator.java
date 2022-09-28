0349eec04e7e2af87457692b237a7f668e7b2a91
ud17 <upandya1297@gmail.com>
2022-09-28 14:07:07 -0400

    easy: single-number (XOR operator)

---
+++ easy/single-number/SolutionOperator.java
@@ -0,0 +1,10 @@
+class SolutionOperator {
+    public int singleNumber(int[] nums) {
+        
+        int res=0;
+        for(int i : nums){
+            res =res ^ i;
+        }
+        return res;
+    }
+}
