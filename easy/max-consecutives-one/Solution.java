43ec5ad89869b6ac99bb966a3adc819f129c8855
ud17 <upandya1297@gmail.com>
2022-09-29 23:50:03 -0400

    easy: max-consecutives-1s

---
+++ easy/max-consecutives-one/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int occurences = 0;
+        int count = 0;
+        
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == 1) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if(count>occurences) {
+                occurences = count;
+            }
+        }
+        return occurences;
+    }
+}
