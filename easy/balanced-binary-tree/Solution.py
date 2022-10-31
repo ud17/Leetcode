964f58c7a4254c2d4dfcfe78ce39c1690c48ee27
ud17 <upandya1297@gmail.com>
2022-10-30 23:32:43 -0400

    easy: balanced-binary-tree (python)

---
+++ easy/balanced-binary-tree/Solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def maxDepth(node):
+            if node is None:
+                return 0
+            left = maxDepth(node.left)
+            right = maxDepth(node.right)
+            return max(left,right) + 1
+        
+        def helper(root):
+            if root is None:
+                return True
+            left_height = maxDepth(root.left)
+            right_height = maxDepth(root.right)
+            
+            if abs(left_height - right_height) > 1:
+                return False
+            left = helper(root.left)
+            right = helper(root.right)
+            
+            if not left or not right:
+                return False
+            return True
+        return helper(root)
